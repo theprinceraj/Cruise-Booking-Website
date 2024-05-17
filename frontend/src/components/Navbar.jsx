e9f0dbda000f3ac8320c39792dfed0c036c72ed3
Ankurcr7 <ankurdas140905@gmail.com>
2024-05-17 14:14:38 +0530

    updateNavbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,43 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import TypographyTheme from './pageTheme';
+
+
+
 export default function Navbar() {
-    return <div>Navbar 2</div>;
+    
+
+    return (<>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Lemin Cruise
+                    </Typography>
+                    <TypographyTheme name={"Home"} />
+                    <TypographyTheme name={"Book"} />
+                    <TypographyTheme name={"Contact"} />
+                    <Button color="inherit">Login/Signup</Button>
+                    
+                </Toolbar>
+            </AppBar>
+        </Box>
+    </>
+
+    );
 }
