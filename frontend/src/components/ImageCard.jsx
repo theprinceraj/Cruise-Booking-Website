a4f75afaadd2e7eafe952a377bad12d7b7d6b271
theprinceraj <profile.princeraj@gmail.com>
2024-05-18 22:43:06 +0530

    "Refactor ImageCard, Navbar, and Home components; update CSS variables"

--- frontend/src/components/ImageCard.jsx
+++ frontend/src/components/ImageCard.jsx
@@ -1,25 +1,5 @@
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
-
 export default function ImageCard() {
-    return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="400"
-                    image="/image1.jpg"
-                    alt="Promotional Card Image"
-                />
-                <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                        Relax in your elegant floating retreat.
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
-    );
+  return (
+    <div>ImageCard</div>
+  )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,31 +1,7 @@
-import { Container } from "@mui/material";
-import ImageCard from "../components/ImageCard";
-import Navbar from "../components/Navbar";
 export default function Home() {
-    return (
-        <>
-            <Navbar />
-            <Container
-                sx={{
-                    backgroundColor: "rgb(0, 0, 0)",
-                    m: 0,
-                    p: 0,
-                }}>
-                <Container
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-evenly",
-                        flexWrap: "wrap",
-                        "& > *": {
-                            margin: "1rem",
-                        },
-                    }}>
-                    <ImageCard />
-                    <ImageCard />
-                    <ImageCard />
-                </Container>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      Home
+    </>
+  )
 }
