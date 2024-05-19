143f8a1bb00d7a778067ca0e34c9d3b6b53f6509
Ankurcr7 <ankurdas140905@gmail.com>
2024-05-19 10:11:23 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -28,11 +28,11 @@ import "../styles/Navbar.css";
 
 export default function CustomNavbar() {
     return (
-        <Navbar expand="lg" className="nav-bg" style={{background:"black"}}>
+        <Navbar expand="lg" className="nav-bg" style={{background:"#303030"}}>
             <Container>
                 <Row className="w-100">
                     <Col xs={12} lg={6} className="d-flex align-items-center">
-                        <Navbar.Brand href="/" style={{color:"red"}}>Lenin Cruise</Navbar.Brand>
+                        <Navbar.Brand href="/" style={{color:"violet"}}>Lenin Cruise</Navbar.Brand>
                     </Col>
                     <Col xs={12} lg={6} className="d-flex justify-content-end">
                         <Nav>
