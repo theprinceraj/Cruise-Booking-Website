9743a39433de62292acdd372dced48efb416437c
theprinceraj <profile.princeraj@gmail.com>
2024-06-03 19:19:12 +0530

    Image Card Addition

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,76 +1,38 @@
 import Navbar from "../components/Navbar.jsx";
+import ImageCard from "../components/ImageCard.jsx";
 export default function Home() {
     return (
         <>
-            <Navbar />
-            <div className="relative object-center object-cover h-[89vh] w-100 overflow-scroll bg-[url('/home-bg.jpg')]  bg-cover bg-center flex items-center justify-center max-h-screen">
-                <div className="flex flex-wrap justify-center items-start">
-                    <div
-                        className="relative !bg-white w-80 m-3 shadow-lg transition-transform transform hover:scale-105">
-                        <div
-                            className="bg-cover bg-center h-52"
-                            style={{ backgroundImage: "url('/image2.avif')" }}></div>
-                        <a
-                            href="#"
-                            className="absolute inset-0 bg-gradient-to-b from-transparent to-transparent hover:to-black/60 transition-all duration-350"></a>
-                        <div className="p-4 relative">
-                            <div className="flex justify-between items-center mb-3">
-                                <a className="bg-green-500 text-white text-xs uppercase px-2 py-1">News</a>
-                                <div className="text-sm text-gray-500">6/11/2018</div>
-                            </div>
-                            <h1 className="text-xl mb-2">There have been big Tesla accident at New Jersey</h1>
-                            <div className="text-gray-600">
-                                by{" "}
-                                <a href="#" className="underline">
-                                    Sardorbek Usmonov
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+            <div className="relative z-[70]">
+                <Navbar />
+            </div>
+            <div className="relative h-[89vh] w-100 flex items-center justify-center max-h-screen">
+                <img
+                    src="/home-bg.jpg"
+                    alt=""
+                    className="w-full h-full object-cover object-center blur-sm brightness-75 absolute l-0 t-0 z-[10]"
+                />
+                <div className="flex flex-wrap justify-center items-start absolute z-[50]">
+                    <ImageCard
+                        imageUrl={"/image2.avif"}
+                        category={"BOOK NOW"}
+                        customText={"There have been big Tesla accident at New Jersey"}
+                        author={"Ankur Das"}
+                    />
 
-                    <div className="relative !bg-white w-80 m-3 shadow-lg transition-transform transform hover:scale-105">
-                        <div
-                            className="bg-cover bg-center h-52"
-                            style={{ backgroundImage: "url('image3.avif')" }}></div>
-                        <a
-                            href="#"
-                            className="absolute inset-0 bg-gradient-to-b from-transparent to-transparent hover:to-black/60 transition-all duration-350"></a>
-                        <div className="p-4 relative">
-                            <div className="flex justify-between items-center mb-3">
-                                <a className="bg-gray-600 text-white text-xs uppercase px-2 py-1">Tech</a>
-                                <div className="text-sm text-gray-500">6/07/2018</div>
-                            </div>
-                            <h1 className="text-xl mb-2">Samsung laptops is exploding again</h1>
-                            <div className="text-gray-600">
-                                by{" "}
-                                <a href="#" className="underline">
-                                    Tyler Platt
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                    <ImageCard
+                        imageUrl={"/image3.avif"}
+                        category={"HURRY UP"}
+                        customText={"Samsung laptops is exploding again"}
+                        author={"Ankit Das"}
+                    />
 
-                    <div className="relative !bg-white w-80 m-3 shadow-lg transition-transform transform hover:scale-105">
-                        <div
-                            className="bg-cover bg-center h-52"
-                            style={{ backgroundImage: "url('image4.avif')" }}></div>
-                        <a
-                            href="#"
-                            className="absolute inset-0 bg-gradient-to-b from-transparent to-transparent hover:to-black/60 transition-all duration-350"></a>
-                        <div className="p-4 relative">
-                            <div className="flex justify-between items-center mb-3">
-                                <a className="bg-yellow-500 text-white text-xs uppercase px-2 py-1">Deals</a>
-                                <div className="text-sm text-gray-500">5/27/2018</div>
-                            </div>
-                            <h1 className="text-xl mb-2">Apple is having big Sale for the first time</h1>
-                            <div className="text-gray-600">
-                                by{" "}
-                                <a href="#" className="underline">
-                                    Prince Raj
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                    <ImageCard
+                        imageUrl={"/image4.avif"}
+                        category={"TIME IS RUNNING OUT"}
+                        customText={"Apple is having big Sale for the first time"}
+                        author={"Prince Raj"}
+                    />
                 </div>
             </div>
         </>
