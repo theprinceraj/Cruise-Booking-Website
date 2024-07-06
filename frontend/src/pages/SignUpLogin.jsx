6137b49bd4d6405dc2a5972215c5e89070413974
Ankurcr7 <ankurdas140905@gmail.com>
2024-07-06 13:05:45 +0530

    update

--- frontend/src/pages/SignUpLogin.jsx
+++ frontend/src/pages/SignUpLogin.jsx
@@ -11,10 +11,10 @@ function SignUpLogin({ isLoginForm }) {
                 <div className="flex items-center justify-center" style={{ marginTop: 60 }}>
                     {/* form changes -> relative removed, height changed(480px), border border-white/10 removed, shadow-xl removed, bg-white/10 removed */}
                     <form
-                        class=" w-[400px] h-[480px]  backdrop-blur-dk rounded-2xl p-12"
+                        className=" w-[400px] h-[480px]  backdrop-blur-dk rounded-2xl p-12"
                         style={{ border: "5px solid grey" }}>
                         <h3
-                            class="text-3xl font-medium text-white text-center"
+                            className="text-3xl font-medium text-white text-center"
                             style={{
                                 color: "white",
                             }}>
@@ -22,8 +22,8 @@ function SignUpLogin({ isLoginForm }) {
                         </h3>
 
                         <label
-                            for="username"
-                            class="block mt-8 text-white text-lg font-medium"
+                            htmlFor="username"
+                            className="block mt-8 text-white text-lg font-medium"
                             style={{ color: "white" }}>
                             Username
                         </label>
@@ -32,12 +32,12 @@ function SignUpLogin({ isLoginForm }) {
                             style={{ backgroundColor: "lightcyan" }}
                             id="username"
                             placeholder="Email or Phone"
-                            class="w-full h-12 mt-2 bg-white/10 rounded-md px-3 text-white placeholder-gray-300 focus:outline-none"
+                            className="w-full h-12 mt-2 bg-white/10 rounded-md px-3 text-white placeholder-gray-300 focus:outline-none"
                         />
 
                         <label
-                            for="password"
-                            class="block mt-8 text-white text-lg font-medium"
+                            htmlFor="password"
+                            className="block mt-8 text-white text-lg font-medium"
                             style={{ color: "white" }}>
                             Password
                         </label>
@@ -46,11 +46,11 @@ function SignUpLogin({ isLoginForm }) {
                             id="password"
                             style={{ backgroundColor: "lightcyan" }}
                             placeholder="Password"
-                            class="w-full h-12 mt-2 bg-white/10 rounded-md px-3 text-white placeholder-gray-300 focus:outline-none"
+                            className="w-full h-12 mt-2 bg-white/10 rounded-md px-3 text-white placeholder-gray-300 focus:outline-none"
                         />
 
                         <button
-                            class="w-full mt-12 bg-white text-[#080710] py-3 rounded-md font-semibold hover:bg-gray-200"
+                            className="w-full mt-12 bg-white text-[#080710] py-3 rounded-md font-semibold hover:bg-gray-200"
                             style={{ color: "white" }}>
                             Submit
                         </button>
@@ -69,42 +69,58 @@ function SignUpLogin({ isLoginForm }) {
                 <div className="flex items-center justify-center" style={{ marginTop: 60 }}>
                     {/* form changes -> relative removed, height changed(480px), border border-white/10 removed, shadow-xl removed, bg-white/10 removed */}
                     <form
-                        class=" w-[400px] h-[480px] bg-white/10 backdrop-blur-dk rounded-2xl  p-12"
+                        className=" w-[420px] h-[540px] bg-white/10 backdrop-blur-dk rounded-2xl  p-12"
                         style={{ border: "5px solid grey" }}>
-                        <h3 class="text-3xl font-medium text-white text-center" style={{ color: "white" }}>
+                        <h3 className="text-3xl font-medium text-white text-center" style={{ color: "white" }}>
                             Signup
                         </h3>
 
                         <label
-                            for="username"
-                            class="block mt-8 text-white text-lg font-medium"
+                            htmlFor="username"
+                            className="block mt-8 text-white text-lg font-medium"
                             style={{ color: "white" }}>
                             Username
                         </label>
                         <input
                             type="text"
                             id="username"
-                            placeholder="Email or Phone"
-                            class="w-full h-12 mt-2 bg-white/10 rounded-md px-3 text-white placeholder-gray-300 focus:outline-none"
+                            name="username"
+                            placeholder="Username"
+                            className="w-full h-12 mt-2 bg-white/10 rounded-md px-3 text-white placeholder-gray-300 focus:outline-none"
+                            style={{ backgroundColor: "lightcyan" }}
+                        />
+                        <label
+                            htmlFor="email"
+                            className="block mt-8 text-white text-lg font-medium"
+                            style={{ color: "white" }}>
+                            Email
+                        </label>
+                        <input
+                            type="text"
+                            id="email"
+                            name="email"
+                            placeholder="Email"
+                            className="w-full h-12 mt-2 bg-white/10 rounded-md px-3 text-white placeholder-gray-300 focus:outline-none"
                             style={{ backgroundColor: "lightcyan" }}
                         />
 
                         <label
-                            for="password"
-                            class="block mt-8 text-white text-lg font-medium"
+                            htmlFor="password"
+                            className="block mt-8 text-white text-lg font-medium"
                             style={{ color: "white" }}>
                             Password
                         </label>
                         <input
                             type="password"
                             id="password"
+                            name="password"
                             style={{ backgroundColor: "lightcyan" }}
                             placeholder="Password"
-                            class="w-full h-12 mt-2 bg-white/10 rounded-md px-3 text-white placeholder-gray-300 focus:outline-none"
+                            className="w-full h-12 mt-2 bg-white/10 rounded-md px-3 text-white placeholder-gray-300 focus:outline-none"
                         />
 
                         <button
-                            class="w-full mt-12 bg-white text-[#080710] py-3 rounded-md font-semibold hover:bg-gray-200"
+                            className="w-full mt-6 bg-white text-[#080710] py-3 rounded-md font-semibold hover:bg-gray-200"
                             style={{ color: "white" }}>
                             Submit
                         </button>
