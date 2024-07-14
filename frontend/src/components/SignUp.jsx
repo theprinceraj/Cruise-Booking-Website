8aab32d8b775e38c4604f60d45ac54fe222a33e0
theprinceraj <profile.princeraj@gmail.com>
2024-07-14 15:58:12 +0530

    .

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -131,15 +131,6 @@ export default function SignUp(){
                     style={{ color: "white" }}>
                     Submit
                 </button>
-
-                {/* <div class="flex justify-center mt-8 space-x-6">
-                    <div class="flex items-center justify-center w-36 h-12 bg-white/30 rounded-md text-white cursor-pointer hover:bg-white/50">
-                        <i class="fab fa-google mr-2"></i> Google
-                    </div>
-                    <div class="flex items-center justify-center w-36 h-12 bg-white/30 rounded-md text-white cursor-pointer hover:bg-white/50">
-                        <i class="fab fa-facebook mr-2"></i> Facebook
-                    </div>
-                </div> */}
             </form>
         </div>
     );
