3d3b97282254f1075bade7146b0b1f45a54be582
theprinceraj <profile.princeraj@gmail.com>
2024-07-14 16:11:57 +0530

    "Added 'isVerified' field to user schema with default value of false"

--- backend/src/models/usermodel.js
+++ backend/src/models/usermodel.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema(
             required: true,
             unique: true,
         },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
     },
     { timestamps: true }
 );
