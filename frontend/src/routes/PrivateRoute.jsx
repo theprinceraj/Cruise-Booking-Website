2f6a076492b534d4c1612b5a6d34e9b2ed8583b6
theprinceraj <profile.princeraj@gmail.com>
2024-07-26 12:38:49 +0530

    "Updated import statement for AuthContext in PrivateRoute.jsx"

--- frontend/src/routes/PrivateRoute.jsx
+++ frontend/src/routes/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import AuthContext from "../contexts/AuthContext";
+import { AuthContext } from "../contexts/AuthContext.jsx";
 
 const PrivateRoute = ({ children }) => {
     const navigate = useNavigate();
