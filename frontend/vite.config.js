9be40dca4a9270d59e30d9c5d35ba1a9d20e3ee1
Ankurcr7 <ankurdas140905@gmail.com>
2024-08-25 10:46:49 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      css: {
+        // Add global CSS files here, these will be prepended to all components
+        additionalData: `
+          @import "./src/styles/Fix_Style.css";
+          @import "./src/styles/React_Calendar_Style.css";
+        `
+      }
+    }
+  }
 })
