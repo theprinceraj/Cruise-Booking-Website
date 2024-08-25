40de02b70ecd676790bc2e8ed4de444314b20a8c
Ankur Das <132298125+Ankurcr7@users.noreply.github.com>
2024-08-25 10:49:27 +0530

    Ankur das v 1.5 (#19)
    * update
    
    * update
    
    * update
    
    * theprinceraj
    
    * .
    
    * update
    
    * userHistory
    
    * userHistory update
    
    * update
    
    * booking calculation update
    
    * update
    
    * UI update
    
    * Major Responsiveness Update
    
    * update
    
    * update
    
    * responsiveness update
    
    * responsiveness update
    
    * update
    
    ---------
    
    Co-authored-by: theprinceraj <profile.princeraj@gmail.com>

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+        './src/styles/Fix_Style.css',
+        './src/styles/React_Calendar_Style.css'
+      ],
     theme: {
         colors: {},
         extend: {},
